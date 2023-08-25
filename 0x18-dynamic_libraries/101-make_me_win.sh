df05e2c4b8ed452da87d1ce0e70e95fa03fae88f
ngobenihc <ngobenihc@yahoo.com>
2023-08-25 11:59:57 +0200

    new a

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/Hagos2022/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
+export LD_PRELOAD=/tmp/gigalib.so
