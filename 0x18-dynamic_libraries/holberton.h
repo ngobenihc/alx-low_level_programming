81b07ed6bfd04674b11e4e8066b05393cb874f07
ngobenihc <ngobenihc@yahoo.com>
2023-08-25 11:31:36 +0200

    new

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTNO_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _atoi(void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -25,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif /* holberton.h */
